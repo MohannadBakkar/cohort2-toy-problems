efeb692138839a06858c19ada021a659703e43fe
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-17 08:32:05 +0200

    done

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -11,14 +11,17 @@
 
    // add an item to the top of the stack
    this.push = function(value){
+   	this.stack.push(value)
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+   	this.stack.pop()
    };
 
    // return the number of items in the stack
    this.size = function(){
+   	return this.stack.length;
    };
  };
 
@@ -29,19 +32,28 @@
    // Use two `stack` instances to implement your `queue` Class
    var inbox = new Stack();
    var outbox = new Stack();
+   var sizeStorage = 0
 
    // called to add an item to the `queue`
    this.enqueue = function(value){
-     
+   	  inbox.push(value)
+   	  outbox.push(value)
+   	  sizeStorage ++
+   	 // outbox.unshift(value)
      }
-   };
+   
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
+   	outbox.reverse()
+   	outbox.pop()
+   	outbox.reverse()
+   	sizeStorage--
     }
-   };
+   
 
    // should return the number of items in the queue
    this.size = function(){
+   	return sizeStorage;
    };
  };
