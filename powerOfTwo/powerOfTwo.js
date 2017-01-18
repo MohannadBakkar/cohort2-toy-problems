cfde15924e33f2e079fd742cd977d8c23b9fe6e9
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-18 09:06:41 +0200

    Toy-problem

--- powerOfTwo/powerOfTwo.js
+++ powerOfTwo/powerOfTwo.js
@@ -9,6 +9,12 @@ var powerOfTwo = function(num){
 	var result = 0
 	var arr = x.split('')
 	for (var i = 0; i < arr.length; i++) {
+		if(arr[i] === '.'){
+			arr.splice(i,1)
+		}
+		if(arr[i] === 'e'){
+			break;
+		}
 		result += Number(arr[i])
 	}
 	return result
