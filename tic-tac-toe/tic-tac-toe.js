fb9b4a1ebd81bff3e4f4840b1b24270e526a8a9d
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-22 09:30:11 +0200

    not finished

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -20,7 +20,64 @@ var solvedBoard = [[0,1,1],
                    [2,1,2]];
 ticTacToe(solvedBoard);//should return 1
 */
+function checkRow(array){
+	var first = array[0]
+	var result
+	for (var i = 0; i < array.length; i++) {
+		if(first !== array[i]){
+			return false
+		}
+	}
+	return first
+}
+
+function checkCol(board){
+	var num = board[0][0]
+	for (var i = 0; i < board.length; i++) {
+		for (var j = 0; j < board.length; j++) {
+			
+			if(board[j][i] !== num){
+				return false
+			}
+		}
+	}
+	return num
+}
+
+
+function checkDaiagoLTR (board){
+	for (var i = 0; i < 1; i++) {
+		if(board[i][0] === board[i+1][1] && board[i+1][1] === board[i+2][2]){
+			return board[i[0]]
+		}
+	}
+	return false
+}
+
+
+function checkDaiagoRTL(board){
+	for (var i = 0; i < 1; i++) {
+		if(board[i][2] === board[i+1][1] && board[i+1][1] === board[i+2][0]){
+			return board[i][2]
+		}
+	}
+	return false
+}
 
 function ticTacToe(board){
+	var result;
+	var num;
+	for (var i = 0; i < board.length; i++) {
+		if(checkRow(board[i]) !== false){
+			return checkRow(board[i])
+		}else if (checkCol(board) !== false){
+			return checkCol(board)
+		}else if(checkDaiagoLTR(board) !== false){
+			return checkDaiagoLTR(board)
+		}else if (checkDaiagoRTL(board) !== false ){
+			return checkDaiagoRTL(board)
+		}
+	}
+	return -1
 	
 };
