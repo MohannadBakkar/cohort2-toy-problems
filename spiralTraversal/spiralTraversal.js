593f8e2a1510f72d5dc07aeabac016adee0ae35b
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-25 09:26:15 +0200

    not solved,

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -8,13 +8,41 @@
       [7,8,9]
     ]);
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
- */
+    */
 
-var spiralTraversal = function(matrix){
-	var arr = []
-	var j = 0
-	for (var i = 0; i < matrix.length; i++) {
-		arr = arr.concat(matrix[i])
-	}
-	return arr
+    var spiralTraversal = function(matrix){
+    	var arr = []
+    	for (var i = 0; i < matrix.length; i++) {
+    		for (var j = 0; j < matrix.length; j++) {
+    			if(i === matrix.length-1){
+    				arr.push(matrix[i].pop())
+    			}else if(i>0){
+    				arr.push(matrix[i].pop())
+    				i++
+    			}else{
+    				arr.push(matrix[i].shift())
+    			}
+    		}
+    	}	
+    	return arr
+
+    };
+var spiralTraversal1 = function(matrixs){
+  var arr = [];
+    var insideArr = function(matrix) {
+        if (matrix.length === 0) {
+            return
+        }
+        arr = arr.concat(matrix.shift());
+        for (var i=1; i < matrix.length - 1; i++) {
+            arr.push(matrix[i].pop());
+        }
+        arr = arr.concat(matrix.pop().reverse());
+        for (var j=matrix.length -2; j > 0; j--) {
+            arr.push(matrix[j].shift());
+        }
+        return insideArr(matrix);
+    };
+    insideArr(matrixs);
+    return arr;
 };
